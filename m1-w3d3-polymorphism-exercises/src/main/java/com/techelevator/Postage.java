06c24a3447734196d85306f2478923ec3d9b379b
Brandon <DeWittInTheRoad@gmail.com>
2018-02-01 09:49:44 -0500

    force o or p

--- m1-w3d3-polymorphism-exercises/src/main/java/com/techelevator/Postage.java
+++ m1-w3d3-polymorphism-exercises/src/main/java/com/techelevator/Postage.java
@@ -6,39 +6,45 @@ import java.util.Scanner;
 
 public class Postage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
-		System.out.print("Please enter the weight of the package? ");
-		int weight = (Integer.parseInt(in.nextLine()));
-		System.out.print("(P)ounds or (O)unces? ");
-		String unit = in.nextLine().toLowerCase();
-		if (unit.equals("p")) weight = convertPoundsToOunces(weight);
-		System.out.print("What distance will it be traveling to? ");
-		int distance = (Integer.parseInt(in.nextLine()));
-		
+    public static void main(String[] args) {
 
-		
-		DeliveryDriver[] rates = new DeliveryDriver[] { new PostalServiceFirstClass(), new PostalServiceSecondClass(), new PostalServiceThirdClass(), new FedEx(), new SPUFourDayGround(), new SPUTwoDayBusiness(), new SPUNextDay()};
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter the weight of the package? ");
+        int weight = (Integer.parseInt(in.nextLine()));
+        String unit = "";
 
-		System.out.println("");
-		System.out.println("");
-		System.out.println(String.format("%-30s %20s", "Delivery Method", "$ cost"));
-		System.out.println("---------------------------------------------------------");
-		for (DeliveryDriver companies : rates) {
-			String output = String.format("%-30s $%20.2f", companies.getName(), companies.calculateRate(distance, weight));
-			
-			System.out.println(output);
-		}
-		System.out.println("---------------------------------------------------------");
-	
-	}
+        do {
+            System.out.print("(P)ounds or (O)unces? "); //pound or ounces only
+            unit = in.nextLine().toLowerCase();
+        }
+        while (!unit.equals("p") && !unit.equals("o"));
 
-	public static int convertPoundsToOunces (int pounds) {
-		int ounces = 0;
-		
-		ounces = (pounds * 16) ;
-		
-		return ounces;
-	}
+        if (unit.equals("p")) weight = convertPoundsToOunces(weight); //all classes are done in ounces
+
+        System.out.print("What distance will it be traveling to? ");
+        int distance = (Integer.parseInt(in.nextLine()));
+
+
+        DeliveryDriver[] rates = new DeliveryDriver[]{new PostalServiceFirstClass(), new PostalServiceSecondClass(), new PostalServiceThirdClass(), new FedEx(), new SPUFourDayGround(), new SPUTwoDayBusiness(), new SPUNextDay()};
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println(String.format("%-30s %20s", "Delivery Method", "$ cost"));
+        System.out.println("---------------------------------------------------------");
+        for (DeliveryDriver companies : rates) {
+            String output = String.format("%-30s $%20.2f", companies.getName(), companies.calculateRate(distance, weight));
+
+            System.out.println(output);
+        }
+        System.out.println("---------------------------------------------------------");
+
+    }
+
+    public static int convertPoundsToOunces(int pounds) {
+        int ounces = 0;
+
+        ounces = (pounds * 16);
+
+        return ounces;
+    }
 }
