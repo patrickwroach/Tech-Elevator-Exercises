bba17456b97db899f0ef8ca9eaf92779dbaf46ef
Brandon <DeWittInTheRoad@gmail.com>
2018-02-02 09:17:19 -0500

    formating

--- m1-w3d3-polymorphism-exercises/src/main/java/com/techelevator/Toll.java
+++ m1-w3d3-polymorphism-exercises/src/main/java/com/techelevator/Toll.java
@@ -4,32 +4,27 @@ import java.util.Random;
 public class Toll {
 
     public static void main(String[] args) {
-        Vehicle[] vehicles = new Vehicle[]{new Car(true), new Car(false), new Truck(4), new Truck (6), new Truck (8), new Tank()};
-
-
         int totalDistance = 0;
         double totalRevenue = 0;
         Random rand = new Random();
-
-
-
+        Vehicle[] vehiclesLit = new Vehicle[]{new Car(true), new Car(false), new Truck(4), new Truck (6), new Truck (8), new Tank()};
 
 
         System.out.println("");
         System.out.println(String.format("%-20s %-15s %6s", "Vehicle", "Travel Distance", "Toll"));
-        System.out.println("----------------------------------------------");
-        for (int i = 0; i < vehicles.length; i ++ ) {
+        System.out.println("------------------------------------------------");
+        for (Vehicle vehicles : vehiclesLit) {
             int distance = rand.nextInt((240 - 10) + 1) + 10;
-            String output = String.format("%-20s %-15d $%6.2f", vehicles[i].getName(), distance, vehicles[i].calculateToll(distance));
+            String output = String.format("%-20s %-17d $%7.2f", vehicles.getName(), distance, vehicles.calculateToll(distance));
             totalDistance += distance;
-            totalRevenue += vehicles[i].calculateToll(distance);
+            totalRevenue += vehicles.calculateToll(distance);
 
             System.out.println(output);
         }
-        System.out.println("----------------------------------------------");
+        System.out.println("------------------------------------------------");
 
-        System.out.println(String.format("%-10s %-10d", "Total Distance Traveled: ", totalDistance));
-        System.out.println(String.format("%-10s $%-10.2f", "Total Revenue: ", totalRevenue));
+        System.out.println(String.format("%-20s %-3d %-1s %-5.2f", "Total Distance: ", totalDistance, "Total Revenue: $", totalRevenue));
+
 
     }
 }
