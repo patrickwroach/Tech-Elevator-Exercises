ae587b4c0edeb7e9c5a7529d5837b3afda22f045
Brandon <DeWittInTheRoad@gmail.com>
2018-03-08 18:24:01 -0500

    done

--- m3-w8d3-sessions-exercises/src/main/java/com/techelevator/FavoriteThingsController.java
+++ m3-w8d3-sessions-exercises/src/main/java/com/techelevator/FavoriteThingsController.java
@@ -1,13 +1,9 @@
 package com.techelevator;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
 
 @Controller
 @SessionAttributes({"favoriteColor", "favoriteSeason", "favoriteFood"})
