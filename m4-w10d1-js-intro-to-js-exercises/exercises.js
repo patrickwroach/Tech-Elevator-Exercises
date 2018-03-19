2c96b76ae9d60b5c1c10108120c6ab56c995e6f5
Brian Lauvray <brian@techelevator.com>
2018-03-19 11:55:15 -0400

    added class examples

--- m4-w10d1-js-intro-to-js-exercises/exercises.js
+++ m4-w10d1-js-intro-to-js-exercises/exercises.js
@@ -1,1 +1,24 @@
-﻿
+﻿/*
+SumDoubleGiventwointvalues,returntheirsum.Unlessthetwovaluesarethesame,thenreturndoubletheirsum.
+sumDouble(1, 2) → 3 sumDouble(3, 2) → 5 sumDouble(2, 2) → 8
+function sumDouble(x, y) { // do logic here
+// return result; return x + y;
+}
+*/
+function sumDouble(x, y) {
+    sum = x + y;
+    if (x === y) {
+        return sum *= 2;
+    }
+    console.log(sum);
+    return sum;
+}
+
+function lastDigit(x, y) {
+    var first = x.toString().split('').pop();
+    var second = y.toString().split('').pop();
+    if (first == second) {
+        return true;
+    }
+    return false;
+}
