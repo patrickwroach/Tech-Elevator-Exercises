9e92cb99c72a9da0caaf79ff20b79876a4fc9668
Brandon <DeWittInTheRoad@gmail.com>
2018-03-20 15:10:23 -0400

    100% complete

--- m4-w10d1-js-intro-to-js-exercises/challenge-exercises.js
+++ m4-w10d1-js-intro-to-js-exercises/challenge-exercises.js
@@ -1,4 +1,25 @@
-﻿
+﻿// function iqTest(arr){
+//     var output = 0;
+//     var evenCount = 0;
+//     var oddCount =0;
+//     var differentIndex = 0;
+//     if (arr.length < 3) return output;
+
+//     for (var i = 0; i < 3; i++){
+//         if (arr[i] %2 == 0) evenCount++;
+//         if (arr[i] %2 == 1) oddCount++;
+//     }
+    
+//     var oddOrEvenCheck = (Math.max(evenCount, oddCount)) ? 0 : 1;
+
+//     for (var i = 0; i < arr.length; i++){
+//         if (arr[i] % 2 != oddOrEvenCheck) differentIndex = i + 1;
+//     }
+
+//     return differentIndex;
+    
+// }
+
 function iqTest(arr){
     
     var evenCount = 0;
