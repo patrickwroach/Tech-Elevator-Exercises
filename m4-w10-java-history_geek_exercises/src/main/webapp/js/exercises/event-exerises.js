ceb9544c22736e3185e4a24c0b1dcdafc7e36ffa
Brandon <DeWittInTheRoad@gmail.com>
2018-03-21 14:14:53 -0400

    day3 part1 done

--- m4-w10-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
+++ m4-w10-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
@@ -1,5 +1,28 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
 
 $(document).ready(function () {
+    $("#SameShipping").click(function(){
+        if (this.checked) {
+            $("#ShippingAddress1").val($("#BillingAddress1").val());
+            $("#ShippingAddress2").val($("#BillingAddress2").val());
+            $("#ShippingCity").val($("#BillingCity").val());
+            $("#ShippingState").val($("#BillingState").val());
+            $("#ShippingPostalCode").val($("#BillingPostalCode").val());
+        }
+        else {
+            $("#ShippingAddress1").val($("").val());
+            $("#ShippingAddress2").val($("").val());
+            $("#ShippingCity").val($("").val());
+            $("#ShippingState").val($("").val());
+            $("#ShippingPostalCode").val($("").val());
+        }
+    })
 
+
+    // var fNameField = $("#BillingAddress1");
+    // fNameField.on("blur", function (event) {
+    //     var blurredField = $(this);
+    //     blurredField.val(blurredField.val().toUpperCase());
+    // });
 });
+
